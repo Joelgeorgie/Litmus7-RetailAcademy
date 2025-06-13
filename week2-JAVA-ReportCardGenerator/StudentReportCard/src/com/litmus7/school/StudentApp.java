cec8c276f268f6385edb0576061e7c56e4329cbd
Joel Georgie <joelvgeorgie@gmail.com>
2025-06-13 13:35:27 +0530

    week2 assignments java

---
+++ week2-JAVA-ReportCardGenerator/StudentReportCard/src/com/litmus7/school/StudentApp.java
@@ -0,0 +1,33 @@
+package com.litmus7.school;
+
+import com.litmus7.school.dto.Student;
+import java.util.Scanner;
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of students: ");
+		int noOfStudents = sc.nextInt();
+		
+		Student students[] = new Student[noOfStudents];
+		
+		
+		for(int i = 0; i<noOfStudents ; i++) {
+			System.out.println("Enter details of Student "+(i+1));
+			students[i]= new Student();
+			students[i].inputDetails();
+		}
+		
+		System.out.println("\nPrinting the Report Cards:\n");
+		
+		for(int i = 0; i<noOfStudents ; i++) {
+			
+			students[i].printReportCard();
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ week2-JAVA-ReportCardGenerator/StudentReportCard/src/com/litmus7/school/dto/Student.java
@@ -0,0 +1,69 @@
+package com.litmus7.school.dto;
+
+import java.util.Scanner;
+
+public class Student {
+	String name ;
+	int rollNumber;
+	double marks[] = new double[5];
+	
+	public void inputDetails() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter student name: ");
+		name = sc.nextLine();
+		
+		System.out.print("Enter roll number: ");
+		rollNumber = sc.nextInt();
+		
+		System.out.println("Enter marks in 5 subjects:");
+		for(int i = 0 ; i<5 ; i++ ) {
+			System.out.print("Subject "+(i+1)+": ");
+			marks[i]=sc.nextDouble();
+		}
+		System.out.println();
+	}	
+	
+	private double calculateTotal() {
+		double total = 0;
+		for(int i=0 ;i<5;i++) {
+			total += marks[i];
+		}
+		
+		return total;
+	}
+	
+	private double calculateAverage() {
+		
+		double averageMarks = calculateTotal()/5 ;
+		return averageMarks;
+	}
+	
+	private char getGrade() {
+		double averageMarks = calculateAverage();
+		
+		if (averageMarks >= 90) {
+			return 'A';
+		}
+		else if (averageMarks >=75 ) {
+			return 'B';
+		}
+		else if (averageMarks >=60 ) {
+			return 'C';
+		}
+		else if (averageMarks >=50 ) {
+			return 'D';
+		}
+
+		return 'F';
+	}
+	
+	public void printReportCard() {
+		System.out.println("--- Report Card ---");
+		System.out.println("Name : "+name);
+		System.out.println("Roll Number : "+rollNumber);
+		System.out.println("Total Marks : "+calculateTotal());
+		System.out.println("Average Marks : "+calculateAverage());
+		System.out.println("Grade : "+getGrade());
+		System.out.println("-------------------");
+	}
+}
